9e535fd3ad1514b32f16e0b0382cc6517baf557c
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-01-01 12:39:11 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
+
+printLogo
+
+printGreen "Install docker and docker compose"
+bash <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/docker-install.sh)
+
+printGreen "Install"
+sudo apt install -y unzip
+wget https://example.com/path/to/openledger-node-1.0.0-linux.zip
+unzip openledger-node-1.0.0-linux.zip -d openledger-node
+
+printGreen "Running node"
+cd openledger-node
+docker-compose up -d
